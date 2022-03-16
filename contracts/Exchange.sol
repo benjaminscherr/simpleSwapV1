456bda472e84023e8dd30ffd553d1c9b105d224e
Ben Scherr <bcscherr@Bens-MacBook-Pro.local>
2022-03-15 22:09:59 -0400

    initial commit

---
+++ contracts/Exchange.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//Each exchange contract represents a ETH - ERC20 token pair exchange
+contract Exchange {
+    
+    //Address of the token, in the ETH - ERC20 token pair
+    address public tokenAddress;
+
+    constructor(address _token) {
+        require(_token != address(0), "invalid token address");
+        tokenAddress = _token;
+    }
+
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    function getReserve() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        return (inputReserve * 1000) / outputReserve;
+}
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("addLiquidity", async () => {
+  it("adds liquidity", async () => {
+    await token.approve(exchange.address, toWei(200));
+    await exchange.addLiquidity(toWei(200), { value: toWei(100) });
+
+    expect(await getBalance(exchange.address)).to.equal(toWei(100));
+    expect(await exchange.getReserve()).to.equal(toWei(200));
+  });
+});
